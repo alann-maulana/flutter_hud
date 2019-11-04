49b9a6752ccd806c594f79d97f349e2ba8c78062
alann-maulana <alann_it06@yahoo.co.id>
2019-11-04 16:40:36 +0700

    Create helper_test.dart

---
+++ test/helper_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hud/src/helper.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_hud/flutter_hud.dart';
+
+void main() {
+  test('test default HUD + value == null', () {
+    final widget = showOrUpdateProgressIndicator(HUD.kDefaultHUD, null);
+
+    expect(widget != null, true);
+    expect(
+        widget is CircularProgressIndicator ||
+            widget is CupertinoActivityIndicator,
+        true);
+  });
+
+  test('test default HUD + value != null', () {
+    final widget = showOrUpdateProgressIndicator(HUD.kDefaultHUD, 0.1);
+
+    expect(widget != null, true);
+    expect(
+        widget is CircularProgressIndicator ||
+            widget is CupertinoActivityIndicator,
+        true);
+  });
+
+  test('test custom HUD', () {
+    final widget = showOrUpdateProgressIndicator(
+        HUD(progressIndicator: CupertinoActivityIndicator()), null);
+
+    expect(widget != null, true);
+    expect(widget is CupertinoActivityIndicator, true);
+
+    final widget2 = showOrUpdateProgressIndicator(
+        HUD(progressIndicator: CupertinoActivityIndicator()), 0.2);
+
+    expect(widget2 != null, true);
+    expect(widget2 is CupertinoActivityIndicator, false);
+    expect(widget2 is CircularProgressIndicator, true);
+  });
+
+  test('test custom HUD + value != null', () {
+    final widget = showOrUpdateProgressIndicator(
+        HUD(progressIndicator: LinearProgressIndicator()), 0.1);
+
+    expect(widget != null, true);
+    expect(
+        widget is CircularProgressIndicator ||
+            widget is CupertinoActivityIndicator,
+        false);
+    expect(widget is LinearProgressIndicator, true);
+  });
+}
