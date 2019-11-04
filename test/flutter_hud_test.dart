6e2f1965761af0f985bae6af0532bf09861318b5
alann-maulana <alann_it06@yahoo.co.id>
2019-11-04 16:40:32 +0700

    Delete flutter_hud_test.dart

--- test/flutter_hud_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_hud/flutter_hud.dart';
-
-void main() {
-  test('adds one to input values', () {
-    expect(true, !false);
-  });
-}
